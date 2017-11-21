7c5be739150d9478cee64f26ee691abcdeb199d7
Gregory Tandiono <gregtandiono@gmail.com>
2017-11-21 15:32:42 +0700

    WIP test setup

--- storm/user_service_test.go
+++ storm/user_service_test.go
@@ -1,1 +1,29 @@
 package storm_test
+
+import (
+	"datwire/pkg/bolt"
+	"testing"
+
+	"github.com/pborman/uuid"
+	"github.com/stretchr/testify/suite"
+)
+
+type UserServiceTestSuite struct {
+	suite.Suite
+	userService *bolt.UserService
+	userID_1    uuid.UUID
+	userID_2    uuid.UUID
+	userID_3    uuid.UUID
+}
+
+func (suite *UserServiceTestSuite) SetupSuite() {
+
+}
+
+func (suite *UserServiceTestSuite) TearDownSuite() {
+
+}
+
+func TestUserServiceSuite(t *testing.T) {
+	suite.Run(t, new(UserServiceTestSuite))
+}

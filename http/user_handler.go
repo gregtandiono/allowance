0de5ddc564df365efab811942e2cf5320056d999
Gregory Tandiono <gregtandiono@gmail.com>
2017-11-21 17:10:12 +0700

    finished user handlers. WIP tests

--- http/user_handler.go
+++ http/user_handler.go
@@ -1,12 +1,15 @@
 package http
 
 import (
+	"allowance"
 	"allowance/storm"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/mux"
+	uuid "github.com/satori/go.uuid"
 )
 
 // UserHandler represents user service REST interface
@@ -23,17 +26,61 @@ func NewUserHandler(dbName string) *UserHandler {
 		Logger:      log.New(os.Stderr, "", log.LstdFlags),
 		UserService: storm.NewUserService(storm.NewClient(dbName)),
 	}
+
+	h.Handle("/users", http.HandlerFunc(h.handleCreateUser)).Methods("POST")
+	h.Handle("/users/{id}", http.HandlerFunc(h.handleGetUser)).Methods("GET")
+	h.Handle("/users/{id}", http.HandlerFunc(h.handleUpdateUser)).Methods("PUT")
+	h.Handle("/users/{id}", http.HandlerFunc(h.handleDeleteUser)).Methods("DELETE")
+
 	return h
 }
 
 func (h *UserHandler) handleCreateUser(w http.ResponseWriter, r *http.Request) {
+	var user allowance.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		EncodeError(w, err, 400, h.Logger)
+	} else if err := h.UserService.CreateUser(&user); err != nil {
+		EncodeError(w, err, 400, h.Logger)
+	} else {
+		EncodeJSON(w, &ResponseTemplate{Message: "success"}, h.Logger)
+	}
 }
 
 func (h *UserHandler) handleGetUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userID := uuid.FromStringOrNil(vars["id"])
+
+	u, err := h.UserService.User(userID)
+	if err != nil {
+		EncodeError(w, err, 400, h.Logger)
+	} else {
+		EncodeJSON(w, &ResponseTemplate{Message: "success", Data: u}, h.Logger)
+	}
 }
 
 func (h *UserHandler) handleUpdateUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userID := uuid.FromStringOrNil(vars["id"])
+
+	var user allowance.User
+	user.ID = userID
+
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		EncodeError(w, err, 400, h.Logger)
+	} else if err := h.UserService.UpdateUser(&user); err != nil {
+		EncodeError(w, err, 400, h.Logger)
+	} else {
+		EncodeJSON(w, &ResponseTemplate{Message: "success"}, h.Logger)
+	}
 }
 
 func (h *UserHandler) handleDeleteUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userID := uuid.FromStringOrNil(vars["id"])
+
+	if err := h.UserService.DeleteUser(userID); err != nil {
+		EncodeError(w, err, 400, h.Logger)
+	} else {
+		EncodeJSON(w, &ResponseTemplate{Message: "success"}, h.Logger)
+	}
 }

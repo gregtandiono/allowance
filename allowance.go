62488789efdbc2097f9ee95de70f5e8f64b93925
Gregory Tandiono <gregtandiono@gmail.com>
2017-11-21 11:38:29 +0700

    initial commit

---
+++ allowance.go
@@ -0,0 +1,13 @@
+package allowance
+
+type User struct{}
+type UserService interface{}
+
+type Company struct{}
+type CompanyService struct{}
+
+type Manager struct{}
+type ManagerService interface{}
+
+type Transaction struct{}
+type TransactionService interface{}

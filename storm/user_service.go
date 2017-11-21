2793bea0cb091ebf6311110515e011983c200d55
Gregory Tandiono <gregtandiono@gmail.com>
2017-11-21 12:28:04 +0700

    WIP user service

--- storm/user_service.go
+++ storm/user_service.go
@@ -1,8 +1,16 @@
 package storm
 
-import "github.com/asdine/storm"
+import (
+	"allowance"
+
+	"github.com/asdine/storm"
+	"github.com/pborman/uuid"
+)
 
 // UserService represents a client to the underlying BoltDB data store.
 type UserService struct {
 	db *storm.DB
 }
+
+// User returns an existing user from DB
+func (s *UserService) User(id uuid.UUID) *allowance.User {}

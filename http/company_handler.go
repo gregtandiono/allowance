a8c51e9350d8dbcb88f9817b0b161e85742e38c6
Gregory Tandiono <gregtandiono@gmail.com>
2017-11-22 11:58:12 +0700

    added handleGetCompany and handleCreateCompany method to company handler

--- http/company_handler.go
+++ http/company_handler.go
@@ -1,11 +1,15 @@
 package http
 
 import (
+	"allowance"
 	"allowance/storm"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gorilla/mux"
+	uuid "github.com/satori/go.uuid"
 )
 
 // CompanyHandler represents company service REST interface
@@ -29,4 +33,35 @@ func NewCompanyHandler(dbName string) *CompanyHandler {
 	// h.Handle("/users/{id}", http.HandlerFunc(h.handleDeleteUser)).Methods("DELETE")
 
 	return h
+}
+
+func (h *CompanyHandler) handleCreateCompany(w http.ResponseWriter, r *http.Request) {
+	var c allowance.Company
+	if err := json.NewDecoder(r.Body).Decode(&c); err != nil {
+		EncodeError(w, err, 400, h.Logger)
+	} else if err := h.CompanyService.CreateCompany(c); err != nil {
+		EncodeError(w, err, 400, h.Logger)
+	} else {
+		EncodeJSON(w, &ResponseTemplate{Message: "success"}, h.Logger)
+	}
+}
+
+func (h *CompanyHandler) handleGetCompany(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	cID := uuid.FromStringOrNil(vars["id"])
+
+	c, err := h.CompanyService.Company(cID)
+	if err != nil {
+		EncodeError(w, err, 400, h.Logger)
+	} else {
+		EncodeJSON(w, &ResponseTemplate{Message: "success", Data: c}, h.Logger)
+	}
+}
+
+func (h *CompanyHandler) handleUpdateCompany(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *CompanyHandler) handleDeleteCompany(w http.ResponseWriter, r *http.Request) {
+
 }
